453023a20cb05916d3d2595ec7bdbb325913162c
Ryan Meier <rymei10@gmail.com>
2016-05-10 20:59:54 -0700

    added restful routes

--- app/controllers/squirell_sitings_controller.rb
+++ app/controllers/squirell_sitings_controller.rb
@@ -7,4 +7,27 @@ class SquirellSitingsController < ApplicationController
   def show
     @squirell = SquirellSiting.find_by(id: params[:id])
   end
+
+  def create
+    @squirell = SquirellSiting.new(id: params[:id], squirell_type: params[:squirell_type], location: params[:location], date: params[:date], description: params[:description])
+    @squirell.save
+    render :show
+  end
+
+   def update
+    @squirell = SquirellSitting.find_by(id: params[:id])
+    @squirell.id = params[:id]
+    @squirell.squirell_type = params[:squirell_type]
+    @squirell.location = params[:location]
+    @squirell.date = params[:date]
+    @squirell.description = params[:description]
+    @squirell.save
+    render :show
+  end
+
+ def destroy
+   @squirell = SquirellSiting.find_by(id: params[:id])
+   @squirell.destroy
+   render json: "SQUIRELL DESTROYED!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
  get 'squirell_siting/:id', to: 'squirell_sitings#show'
   get 'squirell_siting', to: 'squirell_sitings#index'
+  post 'squirell_siting', to: 'squirell_sitings#create'
+  patch 'squirell_siting/:id', to: 'squirell_sitings#update'
+  delete 'squirell_siting/:id', to: 'squirell_sitings#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
